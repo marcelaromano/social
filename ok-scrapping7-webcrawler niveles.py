601883566a123f7b1332e079ee29faa3a301f288
Marcela Romano <romanomarcela@yahoo.com.ar>
2017-08-15 19:14:49 -0400

    Se filtran links en None.

--- ok-scrapping7-webcrawler niveles.py
+++ ok-scrapping7-webcrawler niveles.py
@@ -36,15 +36,36 @@ def obtener_links(url):
     links = set()
     for link in links_beautifulsoup:
         href = link.get('href')
-        links.add(href)
+        if href is not None:
+            links.add(href)
 
     # busqueda de links por expresiones regulares
     links_regex = re.findall('"http[^"]*"', html)
     for link in links_regex:
-        link_sin_comillas = link[1:-1]
-        links.add(link_sin_comillas)
+        if link is not None:
+            link_sin_comillas = link[1:-1]
+            links.add(link_sin_comillas)
 
     return links
+
+# se filtran links no deseados de acuerdo a una blacklist
+def filtrar_links_no_deseados(links):
+    blacklist = ['/login', '/signup']
+    links_filtrados = []
+
+
+    for link in links:
+        hay_que_eliminar = False
+
+        for blackitem in blacklist:
+            if blackitem in link:
+                hay_que_eliminar = True
+                break
+
+        if not hay_que_eliminar:
+            links_filtrados.append(link)
+
+    return links_filtrados
 
 
 root_url = 'https://www.kickstarter.com/discover/advanced?term=Bikes&woe_id=0&sort=magic&seed=2504144&page=1'
@@ -54,8 +75,10 @@ archivo = open('urls.txt', 'w')
 
 links = obtener_links(root_url)
 
+links_filtrados = filtrar_links_no_deseados(links)
+
 # links en la pagina raiz
-for link in links:
+for link in links_filtrados:
     procesar_link(link, urls, archivo)
 
 separador = "------ Termino de recorrer la raiz ------"
@@ -67,8 +90,10 @@ archivo.write(separador + '\n')
 for url in urls:
     links = obtener_links(url)
 
+    links_filtrados = filtrar_links_no_deseados(links)
+
     # links en las paginas de primer nivel de profundidad
-    for link in links:
+    for link in links_filtrados:
         procesar_link(link, urls, archivo)
 
 archivo.close()
